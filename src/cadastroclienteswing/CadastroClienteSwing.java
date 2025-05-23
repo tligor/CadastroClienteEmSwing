cde004c9ab34bbd65f66425c1b14f91a7aae62c0
Igor Santana <santanna.ig@hotmail.com>
2025-05-23 13:29:02 -0300

    instanciada a abertura de tela e opção sair

--- src/cadastroclienteswing/CadastroClienteSwing.java
+++ src/cadastroclienteswing/CadastroClienteSwing.java
@@ -14,7 +14,8 @@ public class CadastroClienteSwing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
     }
     
 }
