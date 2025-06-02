b59398ee2c49dcb6c73f078e9909877590641ede
Igor Santana <santanna.ig@hotmail.com>
2025-06-02 16:28:21 -0300

    Botão de exclusão e seleção de cliente cadastrado

--- src/cadastroclienteswing/TelaPrincipal.java
+++ src/cadastroclienteswing/TelaPrincipal.java
@@ -40,7 +40,8 @@ public class TelaPrincipal extends javax.swing.JFrame {
         lblCpf = new javax.swing.JLabel();
         txtCpf = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
-        clientsTable = new javax.swing.JTable();
+        tabelaClientes = new javax.swing.JTable();
+        btnExcluir = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItemSair = new javax.swing.JMenuItem();
@@ -58,7 +59,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
         lblCpf.setText("CPF:");
 
-        clientsTable.setModel(new javax.swing.table.DefaultTableModel(
+        tabelaClientes.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -69,7 +70,19 @@ public class TelaPrincipal extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(clientsTable);
+        tabelaClientes.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tabelaClientesMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tabelaClientes);
+
+        btnExcluir.setText("Excluir");
+        btnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExcluirActionPerformed(evt);
+            }
+        });
 
         jMenu1.setText("Opções");
 
@@ -92,7 +105,10 @@ public class TelaPrincipal extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnSalvar)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnSalvar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnExcluir))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(lblNome)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -114,7 +130,9 @@ public class TelaPrincipal extends javax.swing.JFrame {
                     .addComponent(lblCpf)
                     .addComponent(txtCpf, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(30, 30, 30)
-                .addComponent(btnSalvar)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSalvar)
+                    .addComponent(btnExcluir))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 370, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(63, Short.MAX_VALUE))
@@ -160,6 +178,38 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btnSalvarActionPerformed
 
+    private void tabelaClientesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelaClientesMouseClicked
+
+    int linhaSelecionada = tabelaClientes.getSelectedRow();
+    Long cpf = (Long) tabelaClientes.getValueAt(linhaSelecionada, 1);
+    
+    Cliente cliente = this.clienteDAO.consultar(cpf);
+    
+    txtNome.setText(cliente.getNome());
+    txtCpf.setText(cliente.getCpf().toString());
+    }//GEN-LAST:event_tabelaClientesMouseClicked
+
+    private void btnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExcluirActionPerformed
+        int linhaSelecionada = tabelaClientes.getSelectedRow();
+        
+        if (linhaSelecionada >= 0) {
+            int result = JOptionPane.showConfirmDialog(this, "Deseja realmente excluir este cliente?", "CUIDADO",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE);
+            if(result == JOptionPane.YES_OPTION){
+                
+                Long cpf = (Long) tabelaClientes.getValueAt(linhaSelecionada, 1);
+                this.clienteDAO.excluir(cpf);
+                modelo.removeRow(linhaSelecionada);
+                
+                JOptionPane.showMessageDialog(null, "Cliente excluido com sucesso", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                limparCampos();
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Nenhum cliente selecionado.", "ERRO", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }//GEN-LAST:event_btnExcluirActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -196,14 +246,15 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnExcluir;
     private javax.swing.JButton btnSalvar;
-    private javax.swing.JTable clientsTable;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItemSair;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblCpf;
     private javax.swing.JLabel lblNome;
+    private javax.swing.JTable tabelaClientes;
     private javax.swing.JTextField txtCpf;
     private javax.swing.JTextField txtNome;
     // End of variables declaration//GEN-END:variables
@@ -220,7 +271,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         modelo.addColumn("Nome");
         modelo.addColumn("CPF");
         
-        clientsTable.setModel(modelo);
+        tabelaClientes.setModel(modelo);
     }
 
     private boolean isClienteCadastrado(String cpf) {
